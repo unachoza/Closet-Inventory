6a4c9b4994024e29eccbeef3c23150bbcc3f9a5e
arianna choza <unachoza@gmail.com>
2026-05-26 03:15:55 -0700

    Added body, shoes, and sleep categories

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -76,6 +76,9 @@ export const carouselCategories = [
 	{ label: "Lingerie", icon: "🎀" },
 	{ label: "Socks", icon: "🧦" },
 	{ label: "Underwear", icon: "🩲" },
+	{ label: "body", icon: "🕴️" },
+	{ label: "Shoes", icon: "👠" },
+	{ label: "Sleep", icon: "😴" },
 ];
 
 export const categoryOptions: Option[] = [
